96fb9d0cc7c20f610232dc4703c0823d20afc7d2
Muneeb Ur Rehman <Muneeb@Amjads-MacBook-Air.local>
2025-11-12 12:20:46 +0500

    fix

--- src/mvc/routes/auth/auth.ts
+++ src/mvc/routes/auth/auth.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import { Request, Response } from "express";
 import { userRoutes } from "../routes-strings";
 const router = express.Router();
 
-router.get(userRoutes?.login, () => "hello");
+const userLogin = async (req: Request, res: Response) => {
+  return res.status(200).json({
+    success: true,
+    token: "here is the token",
+  });
+};
+
+router.get(userRoutes?.login, userLogin);
 
 export default router;
